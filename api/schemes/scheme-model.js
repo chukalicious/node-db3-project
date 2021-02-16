f83cd0faf8807d8e51ad4ef39dc1236f5c4addb3
Katiuska Alicea <katiuskaalicea@gmail.com>
2021-02-15 18:00:21 -0800

    added findById to the scheme-model

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -6,8 +6,13 @@ const db = knex(config.development);
 
 module.exports = {
   find,
+  findById,
 };
 
 function find() {
   return db("schemes");
+}
+
+function findById(id) {
+  return db("schemes").where({ id });
 }
